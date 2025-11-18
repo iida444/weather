955b65a803a0643a316ab3ca3b419bcf27810973
iida444 <iida444.444@gmail.com>
2025-11-18 23:43:23 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+'use strict';
+const weatherForm = document.getElementById('weather-form');
+const cityInput = document.getElementById('city-input');
+const weatherResults = document.getElementById('weather-results');
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  getWeather(cityInput.value);
+});
+
+const getWeather = (city) => {
+  weatherResults.innerHTML = `<div class="loading"></div>`;
+  fetch(
+    `https://api.weatherapi.com/v1/current.json?key=6f225641ddb44c86ac3171409251411&q=${city}&aqi=no`
+  )
+    .then((response) => response.json())
+    .then((jsonData) => {
+      weatherResults.innerHTML = `
+      <div class="results-country">${jsonData.location.country}</div>
+      <div class="results-city">${jsonData.location.name}</div>
+      <div class="results-temp">${jsonData.current.temp_c}<span>°C</span></div>
+      <div class="results-condition">
+          <img src=${'https:' + jsonData.current.condition.icon} alt="icon">
+          <span>${jsonData.current.condition.text}</span>
+      </div>
+    `;
+    });
+};
